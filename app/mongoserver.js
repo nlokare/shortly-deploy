d1a8ac869a1d9c47688089a195e0e704cb7e3f66
Hack Reactor Students <accounts@hackreactor.com>
2015-01-15 16:52:59 -0800

    adds env variable

--- app/mongoserver.js
+++ app/mongoserver.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var connectionString = process.env.CUSTOMCONNSTR_MONGOLAB_URI || 'mongodb://localhost/users';
+var connectionString = process.env.MONGOLAB_URI || 'mongodb://localhost/users';
 mongoose.connect(connectionString);
 
 var db = mongoose.connection;
